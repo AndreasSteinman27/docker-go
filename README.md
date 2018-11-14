50d61273867ff7639dcb794a3a21d2ea0fbc2234
Taylor Daugherty <taylor.daugherty@bettercloud.com>
2018-11-14 09:19:35 -0500

    init

--- README.md
+++ README.md
@@ -2,5 +2,5 @@ Instructions
 
 ```
 docker build -t hello .
-docker run hello
+docker run -p 8080:8080 hello
 ```
--- hello.go
+++ hello.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"github.com/gorilla/mux"
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
 )
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Content-Type", "application/json")
     http.Get("https://jsonplaceholder.typicode.com/todos/1")
 }
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/metrics", GetHandler).Methods("GET")
-	log.Fatal(http.ListenAndServe(":8080", router))
+    router := mux.NewRouter()
+    router.HandleFunc("/", GetHandler).Methods("GET")
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
